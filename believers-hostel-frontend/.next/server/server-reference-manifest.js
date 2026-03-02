afe7dcff9aa65d9db08fbfad755e16fa6f05b05b
Sujan Magar <sujanmagar05825@gmail.com>
2026-03-02 14:09:32 +0545

    add

--- believers-hostel-frontend/.next/server/server-reference-manifest.js
+++ believers-hostel-frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Cc7Ielqp4CP71M+HArIZrU78vBAwiqcxtDNVnxhFZ6s=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
